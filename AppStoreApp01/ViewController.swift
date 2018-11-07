f60994826a7e57653b0c3924b5d16a8ba823cbdd
masato <masato@m-mbp.local>
2018-11-07 17:31:15 +0800

    initial commit

--- AppStoreApp01/ViewController.swift
+++ AppStoreApp01/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FeaturedAppsController: UICollectionViewController {
+
+    private let cellId = "cellId"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        collectionView?.backgroundColor = .white
     }
 
 
+    // Black Cell
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CategoryCell
+        return cell
+    }
+
+
+
+    // 3 Section
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+
+
+}
+
+
+
+
+class CategoryCell: UICollectionViewCell {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setupViews()  {
+
+    }
 }
 
